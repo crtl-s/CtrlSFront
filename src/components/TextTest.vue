d39ca55373f75b4c22507fa78acf0a1c1044aa57
Kvesaa <matejkvesic@gmail.com>
2024-03-09 13:50:40 +0100

    CTRLS-4: TestViews
     Minimal changes in Css. Added view for test and components for Testview

---
+++ src/components/TextTest.vue
@@ -0,0 +1,19 @@
+<template>
+    <div style="min-height: 35vw; min-width: 80vw; border: 2px solid black;" class="m-5">
+        <div>
+            <h1 class="container d-flex flex-column justify-content-center align-items-center m-2" style="color: black;">LEARNING STYLE TEST</h1>
+            <h1 class="container d-flex flex-column justify-content-left align-items-center m-4" style="color: white; background-color: #637bb0; width: 2rem;">1.</h1>
+        </div>
+        <h1 class="container d-flex flex-column justify-content-center align-items-center" style="color: black;">Owls can turn their heads 270 degrees.Owls can turn their heads 270 degrees.Owls can turn their heads 270 degrees.Owls can turn their heads 270 degrees.Owls can turn their heads 270 degrees.Owls can turn their heads 270 degrees.Owls can turn their heads 270 degrees.Owls can turn their heads 270 degrees.Owls can turn their heads 270 degrees</h1>
+        <div class="d-flex justify-content-center align-items-center mb-4" style="margin-top: 5rem;">
+        <button
+            style="width: 10rem; height: 3rem"
+            type="button"
+            class="btn btn-lg btn-custom mx-2"
+            data-bs-toggle="button"
+        >
+            NEXT
+        </button>
+    </div>
+    </div>
+</template>
